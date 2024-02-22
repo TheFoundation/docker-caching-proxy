8d2e6c0bf63eacf233691fcdce48241534f5a6e3
Benjamin Freitag <12718580+benchonaut@users.noreply.github.com>
2024-02-22 16:43:55 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -euo pipefail
 
-sed -i "s|\$MAX_SIZE|${MAX_SIZE:-10g}|" /etc/nginx/nginx.conf
-sed -i "s|\$UPSTREAM|${UPSTREAM}|" /etc/nginx/nginx.conf
+sed -i "s|\$MAX_SIZE|"${MAX_SIZE:-10g}"|" /etc/nginx/nginx.conf
+sed -i "s|\$UPSTREAM|"${UPSTREAM}"|" /etc/nginx/nginx.conf
 sed -i "s|\$GZIP|${GZIP:-on}|" /etc/nginx/nginx.conf
 sed -i "s|\$ALLOWED_ORIGIN|${ALLOWED_ORIGIN:-*}|" /etc/nginx/nginx.conf
 sed -i "s|\$PROXY_READ_TIMEOUT|${PROXY_READ_TIMEOUT:-120s}|" /etc/nginx/nginx.conf
