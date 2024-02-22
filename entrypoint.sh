681b81a019991ee2bac6b5e196bc2a5091837087
Benjamin Freitag <12718580+benchonaut@users.noreply.github.com>
2024-02-22 16:45:35 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,10 +3,10 @@ set -euo pipefail
 
 sed -i "s|\$MAX_SIZE|"${MAX_SIZE:-10g}"|" /etc/nginx/nginx.conf
 sed -i "s|\$UPSTREAM|"${UPSTREAM}"|" /etc/nginx/nginx.conf
-sed -i "s|\$GZIP|${GZIP:-on}|" /etc/nginx/nginx.conf
-sed -i "s|\$ALLOWED_ORIGIN|${ALLOWED_ORIGIN:-*}|" /etc/nginx/nginx.conf
-sed -i "s|\$PROXY_READ_TIMEOUT|${PROXY_READ_TIMEOUT:-120s}|" /etc/nginx/nginx.conf
-sed -i "s|\$MAX_INACTIVE|${MAX_INACTIVE:-60m}|" /etc/nginx/nginx.conf
+sed -i "s|\$GZIP|"${GZIP:-on}"|" /etc/nginx/nginx.conf
+sed -i "s|\$ALLOWED_ORIGIN|"${ALLOWED_ORIGIN:-*}"|" /etc/nginx/nginx.conf
+sed -i "s|\$PROXY_READ_TIMEOUT|"${PROXY_READ_TIMEOUT:-120s}"|" /etc/nginx/nginx.conf
+sed -i "s|\$MAX_INACTIVE|"${MAX_INACTIVE:-60m}"|" /etc/nginx/nginx.conf
 
 [[ -z "$PORT" ]] || sed -i "s|listen 80|listen "$PORT"|" /etc/nginx/nginx.conf
 
